c0fd526f4dddf5bf49048c5aa6d43745055358c1
Rohaan <rohaan.wasim@gmail.com>
2022-07-16 22:22:41 +0000

    Link Login Form To LoginHandlerServlet

--- src/main/webapp/login-script.js
+++ src/main/webapp/login-script.js
@@ -2,15 +2,13 @@ async function validateLogin()
 {
   const email = document.getElementById("email");
   sessionStorage.setItem("email", email.value);
-  const password = document.getElementById("password");
-
-  const submitButton = document.getElementById("login-button");
-  if((email.value != "") && (password.value != ""))
-  {
-    submitButton.disabled = false;
-  }
-  else{
-    submitButton.disabled = true;
-  }
   location.href = 'https://summer22-sps-2.uc.r.appspot.com/';
+  
+  const loginForm = document.getElementById("login-form");
+  loginForm.submit();
 } 
+
+function routeToSignUpPage()
+{
+
+}
