18abe3314c9347299b2aaee189c324a947a4fb10
Yuewen <yuewenkapa@gmail.com>
2022-08-02 15:27:31 -0400

    delete default username; add  info to friend modal

--- src/main/webapp/class-card/name-card.js
+++ src/main/webapp/class-card/name-card.js
@@ -165,7 +165,6 @@ function parseCourseName(course) {
     
     function addClassmates(names){
         for(let i= 0; i < names.length; i++){
-            console.log(i);
             const num  = Math.floor(Math.random() * 8);
             createNameCard(names[i], "images/pic"+ num.toString()+".jpeg");
         }
